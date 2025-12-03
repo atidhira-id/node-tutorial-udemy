6ef72e3b1610dbecae20e8dd930706a17502d645
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-12-03 08:47:24 +0700

    delete unused file

--- section-7/controllers/shop.js
+++
@@ -1,9 +0,0 @@
-const products = require("../data/products");
-
-exports.getShopListPage = (req, res) => {
-  res.render("shop", {
-    products: products,
-    docTitle: "Shop Products",
-    path: "/",
-  });
-};
--- section-7/routes/shop.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const shopController = require("../controllers/shop");
-
-const router = express.Router();
-
-router.get("/", shopController.getShopListPage);
-
-module.exports = router;
