5bf798510cd44d7d5a8a117b0d7b0a5a0b96e198
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-12-05 09:33:33 +0700

    feat: add shop routes

---
+++ section-8/routes/shop.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const shopController = require("../controllers/shop");
+
+const router = express.Router();
+
+router.get("/", shopController.getProducts);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
+
+module.exports = router;
