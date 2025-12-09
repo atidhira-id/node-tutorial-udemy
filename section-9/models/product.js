f3da7c4b6651aedde07e42271377a784c0583af3
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-12-09 10:19:19 +0700

    feat: add new and update existing product

--- section-9/models/product.js
+++ section-9/models/product.js
@@ -14,6 +14,7 @@ const getProductsFromFile = (cb) => {
 
 module.exports = class Product {
   constructor(requestBody) {
+    this.id = requestBody.id ? requestBody.id : null;
     this.title = requestBody.title;
     this.imageUrl = requestBody.imageUrl;
     this.price = requestBody.price;
@@ -22,11 +23,20 @@ module.exports = class Product {
 
   save() {
     getProductsFromFile((products) => {
-      this.id = (products.length + 1).toString();
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (!this.id) {
+        this.id = (products.length + 1).toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      } else {
+        const newProducts = products.map((product) => {
+          return product.id === this.id ? this : product;
+        });
+        fs.writeFile(p, JSON.stringify(newProducts), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
