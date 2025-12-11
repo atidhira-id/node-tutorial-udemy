0e228a4e5b6a2e51ad9aad74781ef8edd50712f3
masatidhira <masatidhira@gmail.com>
2025-12-11 22:36:31 +0700

    feat: use sequelize to create model

--- section-11/models/product.js
+++ section-11/models/product.js
@@ -1,42 +1,29 @@
-const db = require("../utils/database");
-const Cart = require("./cart");
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../utils/database");
 
-module.exports = class Product {
-  constructor(requestBody) {
-    this.id = requestBody.id ? requestBody.id : null;
-    this.title = requestBody.title;
-    this.image_url = requestBody.image_url;
-    this.price = requestBody.price;
-    this.description = requestBody.description;
-  }
+const Product = sequelize.define("product", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
 
-  save() {
-    return db.execute(
-      `INSERT INTO products (title, image_url, price, description) VALUES (?, ?, ?, ?)`,
-      [this.title, this.image_url, this.price, this.description]
-    );
-  }
-
-  update() {
-    return db.execute(
-      `UPDATE products SET title=?, image_url=?, price=?, description=? WHERE products.id=?`,
-      [this.title, this.image_url, this.price, this.description, this.id]
-    );
-  }
-
-  static fetchAll() {
-    return db.execute(`SELECT * FROM products`);
-  }
-
-  static getProductById(productId) {
-    return db.execute(`SELECT * FROM products WHERE products.id = ?`, [
-      productId,
-    ]);
-  }
-
-  static deleteProductById(productId) {
-    return db.execute("DELETE FROM products WHERE products.id = ?", [
-      productId,
-    ]);
-  }
-};
+module.exports = Product;
