6a02b89f28749441a1cf09576749c2f0f6d4248d
masatidhira <masatidhira@gmail.com>
2025-12-11 22:36:52 +0700

    feat: use sequelize to connect to db

--- section-11/utils/database.js
+++ section-11/utils/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const { Sequelize } = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("section11", "root", "atidhira123", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "section10db",
-  password: "atidhira123",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
