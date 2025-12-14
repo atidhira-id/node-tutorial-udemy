b749c24201c455461406fd3412c8df3a6563ab11
masatidhira <masatidhira@gmail.com>
2025-12-14 15:38:04 +0700

    initial commit for section 12

---
+++ section-12/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import mongoConnect from "./utils/database.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// MIDDLEWARE
+app.set("view engine", "pug");
+app.set("views", "views");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+});
