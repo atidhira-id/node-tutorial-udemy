8db2e9eb5ce382051e555eebe1181f1acc148f3f
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-12-16 11:35:39 +0700

    feat: add id and password parameter to mongoConnect

--- section-12/utils/database.js
+++ section-12/utils/database.js
@@ -2,16 +2,26 @@ import mongodb from "mongodb";
 
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = async (callback) => {
+let _db;
+
+export const mongoConnect = async (id, password, callback) => {
   try {
-    const connection = await MongoClient.connect(
-      "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.it4y23n.mongodb.net/?appName=Cluster0"
+    const client = await MongoClient.connect(
+      `mongodb+srv://${id}:${password}@cluster0.it4y23n.mongodb.net/?appName=Cluster0`
     );
 
-    callback(connection);
+    _db = client.db("section12");
+
+    callback();
   } catch (err) {
     console.log(err);
   }
 };
 
-export default mongoConnect;
+export const getDb = () => {
+  return _db ? _db : "No Database";
+};
+
+export const productsDb = () => {
+  return getDb().collection("products");
+};
