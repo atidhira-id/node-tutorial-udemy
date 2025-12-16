2e0c2fc7e66059613eab1d0d1401fe3785fba6d0
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-12-16 11:20:21 +0700

    feat: add routes

--- section-12/index.js
+++ section-12/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
-import mongoConnect from "./utils/database.js";
+import { mongoConnect } from "./utils/database.js";
+import adminRouter from "./routes/admin.js";
+import shopRouter from "./routes/shop.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,7 +17,9 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoConnect((client) => {
-  console.log(client);
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+mongoConnect(() => {
   app.listen(3000);
 });
