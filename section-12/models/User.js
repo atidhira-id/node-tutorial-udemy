0c9f0099fa3a77e4d344ea4e37a036bd408cf8d4
Atidhira Habibillah <atidhiraid68@gmail.com>
2026-01-03 08:55:43 +0700

    feat: add cart and get cart function

--- section-12/models/User.js
+++ section-12/models/User.js
@@ -1,10 +1,12 @@
 import { ObjectId } from "mongodb";
-import { usersDb } from "../utils/database.js";
+import { productsDb, usersDb } from "../utils/database.js";
 
 class User {
-  constructor(name, email) {
+  constructor(id, name, email, cart) {
+    this._id = id;
     this.name = name;
     this.email = email;
+    this.cart = cart;
   }
 
   async save() {
@@ -14,6 +16,65 @@ class User {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  async addToCart(product) {
+    let quantity = 1;
+    const productIndex = this.cart.items.findIndex((item) => {
+      return item.productId.toString() == product._id.toString();
+    });
+
+    const updatedCartItems = [...this.cart.items];
+    if (productIndex >= 0) {
+      quantity = this.cart.items[productIndex].quantity + 1;
+      updatedCartItems[productIndex] = {
+        productId: new ObjectId(product._id),
+        quantity: quantity,
+      };
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: quantity,
+      });
+    }
+
+    const updatedCart = { items: updatedCartItems };
+    try {
+      await usersDb().updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+      console.log("Successfully added to cart");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async getCart() {
+    const cartItems = this.cart.items;
+    const productIds = cartItems.map((item) => item.productId);
+
+    let total = 0;
+    let products = await productsDb()
+      .find({ _id: { $in: productIds } })
+      .toArray();
+
+    products = products.map((product) => {
+      const quantity = cartItems.find(
+        (item) => item.productId.toString() === product._id.toString()
+      ).quantity;
+
+      total += product.price * quantity;
+      return {
+        ...product,
+        quantity: quantity,
+      };
+    });
+
+    return {
+      products: products,
+      total: total,
+    };
   }
 
   static async findById(id) {
