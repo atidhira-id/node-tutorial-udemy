06bf20567edf6bedf674400b9a4d12487f0a0e0f
Atidhira Habibillah <atidhiraid68@gmail.com>
2026-01-03 08:56:08 +0700

    feat: get user data and create user object

--- section-12/index.js
+++ section-12/index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 import { mongoConnect } from "./utils/database.js";
+import User from "./models/User.js";
 import adminRouter from "./routes/admin.js";
 import shopRouter from "./routes/shop.js";
 import { get404Page } from "./controllers/error.js";
@@ -20,6 +21,11 @@ app.set("view engine", "pug");
 app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(async (req, res, next) => {
+  const user = await User.findById("69420788df15b1b9bf2fcf4e");
+  req.user = new User(user._id, user.name, user.email, user.cart);
+  next();
+});
 
 app.use("/admin", adminRouter);
 app.use(shopRouter);
