f71f81aa0cde16ced902c27c7974590a4463e331
Atidhira Habibillah <atidhiraid68@gmail.com>
2026-01-03 08:57:01 +0700

    feat: creatre getCart and postProduct function

--- section-12/controllers/shop.js
+++ section-12/controllers/shop.js
@@ -25,3 +25,30 @@ export const getProductDetail = async (req, res) => {
     console.log(err);
   }
 };
+
+export const getCart = async (req, res) => {
+  try {
+    const cart = await req.user.getCart();
+
+    res.render("shop/cart", {
+      docTitle: "Your Cart",
+      path: "/cart",
+      products: cart.products,
+      total: cart.total,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const postProductToCart = async (req, res) => {
+  const productId = req.body.productId;
+  try {
+    const user = req.user;
+    const product = await Product.getProductById(productId);
+    await user.addToCart(product);
+    res.redirect("/cart");
+  } catch (err) {
+    console.log(err);
+  }
+};
