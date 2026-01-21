7b0524ac9fb679437a13dc28dc39d5d8684ab450
Atidhira Habibillah <atidhiraid68@gmail.com>
2026-01-21 16:39:28 +0700

    feat: add mongoose

--- section-25-rest-api-advance/be/index.js
+++ section-25-rest-api-advance/be/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const feedRoutes = require("./routes/feed");
 
 const app = express();
@@ -10,6 +11,19 @@ app.use(cors());
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://atidhiraid68:atidhiraid68@cluster0.it4y23n.mongodb.net/?appName=Cluster0",
+    );
+    console.log("Connected to MongoDB");
+
+    app.listen(8080, () => {
+      console.log("Server is running on port 8080");
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+connectDB();
