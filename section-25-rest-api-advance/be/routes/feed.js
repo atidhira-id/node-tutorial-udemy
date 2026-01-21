c72b680ff7c273304c30402fe87bcb7e503f4477
Atidhira Habibillah <atidhiraid68@gmail.com>
2026-01-21 16:40:15 +0700

    feat: add validator

--- section-25-rest-api-advance/be/routes/feed.js
+++ section-25-rest-api-advance/be/routes/feed.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const { body } = require("express-validator");
+
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
-router.get("/posts", feedController.getFeed);
-router.post("/post", feedController.createFeed);
+router.get("/posts", feedController.getPosts);
+router.post(
+  "/post",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage("Title must be at least 5 characters long"),
+    body("content")
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage("Content must be at least 5 characters long"),
+  ],
+  feedController.createPost,
+);
 
 module.exports = router;
