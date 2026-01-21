314cd8b401bc04f4df20d5e40fe67b315400a8b9
Atidhira Habibillah <atidhiraid68@gmail.com>
2026-01-21 16:39:56 +0700

    feat: add validator

--- section-25-rest-api-advance/be/controllers/feed.js
+++ section-25-rest-api-advance/be/controllers/feed.js
@@ -1,15 +1,41 @@
-exports.getFeed = (req, res) => {
+const { validationResult } = require("express-validator");
+
+exports.getPosts = (req, res) => {
   res.status(200).json({
-    message: "Feed retrieved successfully",
-    feeds: [{ id: 1, title: "First Feed", content: "This is the first feed" }],
+    message: "Posts retrieved successfully",
+    posts: [
+      {
+        id: 1,
+        title: "First Post",
+        content: "This is the first post",
+        imageUrl: "images/duck.jpg",
+        creator: { id: 1, name: "John Doe" },
+        createAt: new Date(),
+      },
+    ],
   });
 };
 
-exports.createFeed = (req, res) => {
+exports.createPost = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed",
+      errors: errors.array(),
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
-    message: "Feed created successfully",
-    feed: { id: 2, title: title, content: content },
+    message: "Post created successfully",
+    post: {
+      id: 2,
+      title: title,
+      content: content,
+      imageUrl: "images/duck.jpg",
+      creator: { id: 1, name: "John Doe" },
+      createAt: new Date(),
+    },
   });
 };
